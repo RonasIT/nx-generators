a15b7069b2c384108d13ed3717fba26dc60fa760
vmikhalevskii <vmikhalevskii@ronasit.com>
2024-03-26 17:10:20 +0300

    feat: directory validation

--- plugin/src/generators/react-component/generator.ts
+++ plugin/src/generators/react-component/generator.ts
@@ -2,16 +2,26 @@ import { formatFiles, generateFiles, Tree } from '@nx/devkit';
 import * as path from 'path';
 import { ReactComponentGeneratorSchema } from './schema';
 import { askQuestion, convertToKebabCase, formatName } from '../../shared/utils';
+import * as fs from 'fs';
 
 export async function reactComponentGenerator(
   tree: Tree,
   options: ReactComponentGeneratorSchema
 ) {
   options.directory = options.directory || await askQuestion('Enter the directory of the library: ');
+  const libPath = `libs/${options.directory}`
+
+  if (!fs.existsSync(libPath)) {
+    console.error(`Directory ${libPath} does not exist!`);
+    process.exit(1);
+  }
+
   options.name = options.name || await askQuestion('Enter the name of the component (e.g: AppButton): ');
 
-  const libPath = `libs/${options.directory}/src/lib`;
-  const componentPath = options.subcomponent ? libPath : `${libPath}/${convertToKebabCase(options.name)}`;
+  const libRootPath = `${libPath}/src/lib`;
+  const componentPath = options.subcomponent
+    ? libRootPath
+    : `${libRootPath}/${convertToKebabCase(options.name)}`;
 
   generateFiles(
     tree,

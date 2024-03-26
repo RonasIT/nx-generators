a1032ea358c6295b87f971e97313d50d6d409813
vmikhalevskii <vmikhalevskii@ronasit.com>
2024-03-26 12:08:05 +0300

    fix: change react-lib functionality

--- plugin/src/generators/react-lib/generator.ts
+++ plugin/src/generators/react-lib/generator.ts
@@ -2,23 +2,30 @@ import { formatFiles, generateFiles, Tree } from '@nx/devkit';
 import * as path from 'path';
 import { ReactLibGeneratorSchema } from './schema';
 import { execSync } from 'child_process';
-import { formatName } from '../../shared/utils';
+import { formatName, getLibName, askQuestion } from '../../shared/utils';
 
 export async function reactLibGenerator(
   tree: Tree,
   options: ReactLibGeneratorSchema
 ) {
+  if (!options.directory) {
+    options.app = await askQuestion("Enter the name of the app (e.g: mobile) or 'shared': ")
+    options.scope = await askQuestion("Enter the scope (e.g: profile) or 'shared': ")
+    options.type = await askQuestion("Enter the type (utils, ui, data-access or features): ")
+    options.name = await askQuestion("Enter the name of the library (e.g: profile-settings): ")
+  }
+
   const libPath = options.directory
     ? `libs/${options.directory}`
     : `libs/${options.app}/${options.scope}/${options.type}/${options.name}`;
-  const srcPath = `${libPath}/src`
   const command = `npx nx g @nx/expo:lib --skipPackageJson --unitTestRunner=none --projectNameAndRootFormat=derived ${libPath}`
   const commandWithOptions = options.dryRun ? command + ' --dry-run' : command
 
   execSync(commandWithOptions, { stdio: 'inherit' });
 
   if (options.withComponent) {
-    generateFiles(tree, path.join(__dirname, 'files'), srcPath, { ...options, formatName });
+    const srcPath = `${libPath}/src`
+    generateFiles(tree, path.join(__dirname, 'files'), srcPath, { ...options, name: getLibName(libPath), formatName });
     tree.write(`${srcPath}/index.ts`, "export * from './lib/component';");
   }
 
--- plugin/src/shared/utils.ts
+++ plugin/src/shared/utils.ts
@@ -1,3 +1,5 @@
+import * as readline from 'readline';
+
 export const formatName = (value: string, withoutSpaces = false) =>
   value
     .split('-')
@@ -9,3 +11,19 @@ export const getProjectName = (str: string) => {
 
   return parts.length > 1 ? parts[1].split('/')[0] : parts[0];
 };
+
+export const getLibName = (path: string) => path.split('/').pop();
+
+export const askQuestion = (question: string): Promise<string> => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  return new Promise((resolve) =>
+    rl.question(question, (answer) => {
+      rl.close();
+      resolve(answer);
+    })
+  );
+}

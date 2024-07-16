c0f839bd91448418147dda014102d76376560ab0
vaniasuveev <vania-suveev@mail.ru>
2024-07-16 15:34:06 +0300

    feat: update next generator to use next-intl package

--- plugin/src/generators/next-app/generator.ts
+++ plugin/src/generators/next-app/generator.ts
@@ -1,11 +1,25 @@
 import { execSync } from 'child_process';
-import { installPackagesTask, Tree } from '@nx/devkit';
+import {
+  addDependenciesToPackageJson,
+  formatFiles,
+  generateFiles,
+  installPackagesTask,
+  readJson,
+  Tree,
+  writeJson,
+} from '@nx/devkit';
 import { NextAppGeneratorSchema } from './schema';
 import { existsSync } from 'fs';
+import { formatName } from '../../shared/utils';
+import * as path from 'path';
+
+const dependencies = {
+  'next-intl': '^3.17.2',
+};
 
 export async function nextAppGenerator(
   tree: Tree,
-  options: NextAppGeneratorSchema
+  options: NextAppGeneratorSchema,
 ) {
   const appRoot = `apps/${options.directory}`;
 
@@ -14,12 +28,36 @@ export async function nextAppGenerator(
 
   if (!existsSync(appRoot)) {
     execSync(
-      `npx nx g @nx/next:app ${options.name} --directory=apps/${options.directory} --projectNameAndRootFormat=as-provided --style=scss --src=false --unitTestRunner=none --e2eTestRunner=none`
+      `npx nx g @nx/next:app ${options.name} --directory=apps/${options.directory} --projectNameAndRootFormat=as-provided --appDir=true --style=scss --src=false --unitTestRunner=none --e2eTestRunner=none`,
+      { stdio: 'inherit' },
     );
   }
 
   // Remove unnecessary files and files that will be replaced
   tree.delete(`${appRoot}/public/.gitkeep`);
+  tree.delete(`${appRoot}/app/api`);
+  tree.delete(`${appRoot}/app/page.tsx`);
+  tree.delete(`${appRoot}/app/page.module.scss`);
+  tree.delete(`${appRoot}/app/global.css`);
+  tree.delete(`${appRoot}/app/layout.tsx`);
+  tree.delete(`${appRoot}/.eslintrc.json`);
+
+  // Update app tsconfig.json to skip automatic reconfiguration during the first application run
+  const appTsconfigPath = `${appRoot}/tsconfig.json`;
+  const appTsconfigJson = readJson(tree, appTsconfigPath);
+  appTsconfigJson.include.push('.next/types/**/*.ts');
+  writeJson(tree, appTsconfigPath, appTsconfigJson);
+
+  // Add app files
+  generateFiles(tree, path.join(__dirname, 'files'), appRoot, {
+    ...options,
+    formatName,
+  });
+
+  // Add dependencies
+  addDependenciesToPackageJson(tree, dependencies, {});
+
+  await formatFiles(tree);
 
   return () => {
     installPackagesTask(tree);

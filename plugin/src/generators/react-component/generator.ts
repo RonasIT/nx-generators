b728a5eeab2b3c99bfbf2276c9683d26a59be790
Dmitry Degtyarev <ddegtyarev@ronasit.com>
2024-08-19 17:48:47 +0600

    chore(react-component): fix esm import, update implementation

--- plugin/src/generators/react-component/generator.ts
+++ plugin/src/generators/react-component/generator.ts
@@ -3,14 +3,18 @@ import * as path from 'path';
 import { kebabCase } from 'lodash';
 import { ReactComponentGeneratorSchema } from './schema';
 import { askQuestion, formatName, getNxLibsPaths, LibraryType, searchNxLibsPaths } from '../../shared/utils';
+import { existsSync } from 'fs';
+
+const dynamicImport = new Function(
+  'specifier',
+  'return import(specifier)'
+) as <T = never>(specifier: string) => Promise<T>;
 
 export async function reactComponentGenerator(
   tree: Tree,
   options: ReactComponentGeneratorSchema
 ) {
-
-  // TODO: Resolve the problem with import
-  const { default: autocomplete } = await import('inquirer-autocomplete-standalone');
+  const { default: autocomplete } = await dynamicImport<typeof import('inquirer-autocomplete-standalone')>('inquirer-autocomplete-standalone');
 
   const nxLibsPaths = getNxLibsPaths([LibraryType.FEATURES, LibraryType.UI]);
 
@@ -24,13 +28,40 @@ export async function reactComponentGenerator(
   })
 
   options.name = options.name || await askQuestion('Enter the name of the component (e.g: AppButton): ');
+  options.subcomponent = options.subcomponent || await askQuestion('Generate component inside components folder? (y/n): ') === 'y';
 
   const libRootPath = `${libPath}/lib`;
   const componentPath = options.subcomponent
     ? `${libRootPath}/components/${kebabCase(options.name)}`
     : libRootPath;
+  const shouldUpdateLibIndexes = !existsSync(libRootPath);
 
   generateFiles(tree, path.join(__dirname, `files`), componentPath, { ...options, formatName });
+
+  const updateIndexes = (): void => {
+    const componentsIndexFilePath = `${libRootPath}/components/index.ts`;
+
+    if (shouldUpdateLibIndexes) {
+      const libIndexFilePath = `${libPath}/index.ts`;
+      const libIndexFileContent = tree.read(libIndexFilePath, 'utf-8');
+      const contentUpdate = libIndexFileContent + `export * from './lib';\n`;
+
+      tree.write(libIndexFilePath, contentUpdate);
+    }
+
+    if (!existsSync(componentsIndexFilePath)) {
+      tree.write(componentsIndexFilePath, `export * from './${kebabCase(options.name)}';\n`);
+    } else {
+      const componentsIndexFileContent = tree.read(componentsIndexFilePath, 'utf-8');
+      const contentUpdate = componentsIndexFileContent + `export * from './${kebabCase(options.name)}';\n`;
+
+      tree.write(componentsIndexFilePath, contentUpdate);
+    }
+  }
+
+  if (options.subcomponent) {
+    updateIndexes();
+  }
 
   await formatFiles(tree);
 }

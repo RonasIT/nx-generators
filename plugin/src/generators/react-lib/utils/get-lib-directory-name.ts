3f0bc6d04a673a3c4c505cfbb17c7b94f0e87f2f
ekazankova <ekazankova@ronasit.com>
2024-10-07 11:26:26 +0600

    feat: removed check of singular words of scope name

--- plugin/src/generators/react-lib/utils/get-lib-directory-name.ts
+++ plugin/src/generators/react-lib/utils/get-lib-directory-name.ts
@@ -6,11 +6,6 @@ export function getLibDirectoryName(libName: string, scopeName: string): string 
   const startsWithScopeWords = scopeWords.every((word, index) => libWords[index] === word);
   if (startsWithScopeWords && libWords.length > scopeWords.length) {
     newLibWords = libWords.slice(scopeWords.length);
-  } else {
-    const index = libWords.findIndex((libWord) => !scopeWords.includes(libWord));
-    if (index > -1) {
-      newLibWords = libWords.slice(index);
-    }
   }
 
   return newLibWords.join('-');

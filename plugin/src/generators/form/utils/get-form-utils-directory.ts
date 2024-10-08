c46ee75b9fd400a78b002dcfe3666c0286ca7def
ekazankova <ekazankova@ronasit.com>
2024-10-08 15:36:27 +0600

    chore: changed variables names

--- plugin/src/generators/form/utils/get-form-utils-directory.ts
+++ plugin/src/generators/form/utils/get-form-utils-directory.ts
@@ -16,26 +16,26 @@ function getFormUtilsPaths(): Array<string> {
   return searchNxLibsPaths(utilsLibsPaths, 'utils/form/src', 'endsWith');
 }
 
-export async function getFormUtilsDirectory(tree: Tree, formApp: string): Promise<string> {
+export async function getFormUtilsDirectory(tree: Tree, appName: string): Promise<string> {
   const { default: autocomplete } = await dynamicImport<typeof import('inquirer-autocomplete-standalone')>('inquirer-autocomplete-standalone');
 
   const formUtilsLibsPaths = getFormUtilsPaths();
 
   if (!formUtilsLibsPaths.length) {
-    const appDirectory = await selectApplication(tree, 'It\'s necessary to generate form utilities. What application should they be in?')
-    await runFormUtilsGenerator(tree, { directory: appDirectory });
+    const formUtilsAppDirectory = await selectApplication(tree, 'It\'s necessary to generate form utilities. What application should they be in?')
+    await runFormUtilsGenerator(tree, { directory: formUtilsAppDirectory });
 
     return searchAliasPath(getFormUtilsPaths()[0]);
   }
 
   if (formUtilsLibsPaths.length > 1) {
-    if (formApp === constants.sharedValue) {
+    if (appName === constants.sharedValue) {
       return searchAliasPath(formUtilsLibsPaths.find((path) => getAppName(path) === constants.sharedValue))
     }
 
     formUtilsLibsPaths[0] = await autocomplete({
       message: 'Select the path of the library with the form utilities: ',
-      source: (input) => filterSource(input, formUtilsLibsPaths.filter((path) => [formApp, constants.sharedValue].includes(path.split('/')[1])))
+      source: (input) => filterSource(input, formUtilsLibsPaths.filter((path) => [appName, constants.sharedValue].includes(getAppName(path))))
     });
   }
 

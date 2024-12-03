0c8e355509be945029f150878980af03c5c559eb
ekazankova <ekazankova@ronasit.com>
2024-12-03 19:09:44 +0700

    fix: add \r after answer

--- plugin/src/shared/utils/cli-utils.ts
+++ plugin/src/shared/utils/cli-utils.ts
@@ -18,6 +18,7 @@ export const askQuestion = (question: string, defaultAnswer?: string): Promise<s
 
   return new Promise((resolve) =>
     rl.question(question, (answer) => {
+      rl.write('\r');
       rl.close();
       resolve(answer);
     }),

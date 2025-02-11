c862d3e4e61d6b03c78aeacc3b0c93527b6bf31a
Dmitry Degtyarev <ddegtyarev@ronasit.com>
2025-02-11 13:44:21 +0600

    chore(config-utils): migrate to eslint flat config

--- plugin/src/generators/lib-tags/generator.ts
+++ plugin/src/generators/lib-tags/generator.ts
@@ -1,19 +1,13 @@
 import { execSync } from 'child_process';
 import { Tree, getProjects, ProjectConfiguration, formatFiles, output } from '@nx/devkit';
 import { noop } from 'lodash';
-import { askQuestion, getNxRules, readESLintConfig, verifyEsLintConfig } from '../../shared/utils';
+import { askQuestion, verifyEsLintConstraintsConfig } from '../../shared/utils';
 import { LibTagsContext } from './interfaces';
 import { LibTagsGeneratorSchema } from './schema';
 import { checkApplicationTags, checkLibraryTags } from './utils';
 
 const context: LibTagsContext = {
-  config: {},
-  rules: [],
-  log: console.log,
-  reload: (tree: Tree) => {
-    context.config = readESLintConfig(tree).config;
-    context.rules = getNxRules(context.config);
-  },
+  log: console.log
 };
 
 export async function libTagsGenerator(tree: Tree, options: LibTagsGeneratorSchema): Promise<void> {
@@ -34,15 +28,14 @@ export async function libTagsGenerator(tree: Tree, options: LibTagsGeneratorSche
 
   // #1 Check eslint config nx-boundaries rule
   output.log({ title: output.bold('1. Checking eslint config nx-boundaries rule...') });
-  context.config = verifyEsLintConfig(tree);
+  verifyEsLintConstraintsConfig(tree);
 
-  // #2 Check projects tags
-  output.log({ title: output.bold('2. Checking projects tags...') });
+  // #2 Check project tags
+  output.log({ title: output.bold('Checking projects tags...') });
+
   const projects = getProjects(tree);
   const applications: Array<ProjectConfiguration> = [];
   const libraries: Array<ProjectConfiguration> = [];
-
-  context.rules = getNxRules(context.config);
 
   projects.forEach((project) => {
     if (project.projectType === 'application') {
@@ -55,9 +48,6 @@ export async function libTagsGenerator(tree: Tree, options: LibTagsGeneratorSche
   });
 
   applications.forEach((application) => checkApplicationTags(application, tree, context));
-
-  context.reload(tree);
-
   libraries.forEach((library) => checkLibraryTags(library, tree, context));
 
   await formatFiles(tree);

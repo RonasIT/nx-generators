c6fe68e358e6dccd389de7f00e43c9985f8d6394
Dmitry Degtyarev <ddegtyarev@ronasit.com>
2025-02-19 13:18:10 +0600

    chore(code-checks): update scripts

--- plugin/src/generators/code-checks/scripts.ts
+++ plugin/src/generators/code-checks/scripts.ts
@@ -1,5 +1,5 @@
 export default {
-  lint: 'npx tsc && cross-env ESLINT_USE_FLAT_CONFIG=false npx eslint ./',
+  lint: 'npx tsc && npx eslint ./',
   format: 'npx prettier --write . && npm run lint -- --fix',
   prepare: 'husky',
 };

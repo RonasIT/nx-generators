b23bc58fc59ca0e3e428032ba4b36ba15df50b9a
ekazankova <ekazankova@ronasit.com>
2025-05-21 17:47:33 +0600

    feat: navigation utils for an expo app

--- plugin/src/shared/generators/navigation-utils/generator.ts
+++ plugin/src/shared/generators/navigation-utils/generator.ts
@@ -1,12 +1,13 @@
 import { execSync } from 'child_process';
 import * as path from 'path';
 import { formatFiles, generateFiles, Tree } from '@nx/devkit';
+import { NavigationUtilsGeneratorSchema } from './schema';
 
-export async function runNavigationUtilsGenerator(tree: Tree, options: { directory: string }): Promise<void> {
-  const libRoot = `libs/${options.directory}`;
+export async function runNavigationUtilsGenerator(tree: Tree, options: NavigationUtilsGeneratorSchema): Promise<void> {
+  const libRoot = `libs/${options.appDirectory}`;
 
   // Generate shared libs
-  execSync(`npx nx g react-lib --app=${options.directory} --scope=shared --type=utils --name=navigation`, {
+  execSync(`npx nx g react-lib --app=${options.appDirectory} --scope=shared --type=utils --name=navigation`, {
     stdio: 'inherit',
   });
 
@@ -14,7 +15,7 @@ export async function runNavigationUtilsGenerator(tree: Tree, options: { directo
   tree.delete(`${libRoot}/shared/utils/navigation/src/index.ts`);
 
   // Add lib files
-  generateFiles(tree, path.join(__dirname, '/lib-files'), libRoot, {});
+  generateFiles(tree, path.join(__dirname, `/${options.baseGeneratorType}-lib-files`), libRoot, {});
 
   await formatFiles(tree);
 }

03997a64a5db302aa4a3e332a0de529f2670c8cf
tfomkin <tfomkin@ronasit.com>
2025-06-23 22:02:02 +0300

    chore: update dependencies versions

--- plugin/src/shared/dependencies.ts
+++ plugin/src/shared/dependencies.ts
@@ -1,77 +1,75 @@
 export const dependencies = {
   'expo-app': {
-    '@ronas-it/react-native-common-modules': '^7.0.0',
+    '@ronas-it/react-native-common-modules': '^0.7.0',
     'react-native-mmkv': '^3.2.0',
-    'expo-constants': '~17.0.3',
-    'expo-dev-client': '^5.0.6',
-    'expo-router': '~4.0.14',
-    'react-native-safe-area-context': '^4.12.0',
-    'react-native-screens': '~4.4.0',
-    'expo-linking': '~7.0.3',
-    'expo-status-bar': '^2.0.0',
-    'expo-updates': '^0.26.10',
-    'expo-insights': '~0.8.1',
+    'expo-constants': '17.1.6',
+    'expo-dev-client': '^5.2.0',
+    'expo-router': '~5.1.0',
+    'react-native-safe-area-context': '^5.4.0',
+    'react-native-screens': '~4.11.1',
+    'expo-linking': '~7.1.5',
+    'expo-status-bar': '^2.2.3',
+    'expo-updates': '^0.28.14',
+    'expo-insights': '~0.9.3',
     'lodash-es': '^4.17.21',
   },
   'expo-app-root': {
-    // Need new version to fix this error:
-    // https://github.com/kristerkari/react-native-svg-transformer/issues/329
-    'react-native-svg-transformer': '^1.4.0',
+    'react-native-svg-transformer': '^1.5.1',
   },
   'next-app': {
-    'next-intl': '^3.26.3',
+    'next-intl': '^4.1.0',
     'lodash-es': '^4.17.21',
     'class-transformer': '^0.5.1',
   },
   'api-client': {
-    '@ronas-it/axios-api-client': '^0.1.0',
+    '@ronas-it/axios-api-client': '^0.1.2',
   },
   auth: {
-    luxon: '^3.4.4',
+    luxon: '^3.6.1',
   },
   'rn-styles': {
     'react-native-extended-stylesheet': '^0.12.0',
   },
   store: {
     '@ronas-it/rtkq-entity-api': '^0.4.11',
-    'react-redux': '^9.1.2',
+    'react-redux': '^9.2.0',
   },
   'ui-kitten': {
     '@eva-design/eva': '^2.2.0',
     '@ui-kitten/components': '^5.3.1',
   },
   form: {
-    '@hookform/resolvers': '^3.9.0',
-    'react-hook-form': '^7.53.0',
-    yup: '^1.4.0',
+    '@hookform/resolvers': '^5.1.1',
+    'react-hook-form': '^7.57.0',
+    yup: '^1.6.1',
   },
   sentry: {
     expo: {
-      '@sentry/react-native': '~6.6.0',
+      '@sentry/react-native': '~6.14.0',
     },
     next: {
-      '@sentry/nextjs': '^8.54.0',
+      '@sentry/nextjs': '^9.28.1',
     },
   },
 };
 
 export const devDependencies = {
   'code-checks': {
-    '@eslint/compat': '^1.2.6',
-    '@eslint/eslintrc': '^3.2.0',
-    eslint: '^9.8.0',
-    prettier: '^3.3.2',
-    'eslint-config-prettier': '^9.1.0',
-    'eslint-import-resolver-typescript': '^3.7.0',
-    'eslint-plugin-import': '^2.29.1',
-    'eslint-plugin-jsx-a11y': '^6.9.0',
-    'eslint-plugin-react': '^7.34.3',
-    'eslint-plugin-react-hooks': '^5.0.0',
+    '@eslint/compat': '^1.3.0',
+    '@eslint/eslintrc': '^3.3.1',
+    eslint: '^9.28.0',
+    prettier: '^3.5.3',
+    'eslint-config-prettier': '^10.1.5',
+    'eslint-import-resolver-typescript': '^4.4.3',
+    'eslint-plugin-import': '^2.31.0',
+    'eslint-plugin-jsx-a11y': '^6.10.2',
+    'eslint-plugin-react': '^7.37.5',
+    'eslint-plugin-react-hooks': '^5.2.0',
     'eslint-plugin-unused-imports': '^4.1.4',
-    husky: '^9.1.5',
-    '@stylistic/eslint-plugin': '^2.12.1',
-    '@typescript-eslint/eslint-plugin': '^8.18.1',
-    '@typescript-eslint/parser': '^8.18.1',
+    husky: '^9.1.7',
+    '@stylistic/eslint-plugin': '^4.4.1',
+    '@typescript-eslint/eslint-plugin': '^8.34.0',
+    '@typescript-eslint/parser': '^8.34.0',
     'tsc-files': '^1.1.4',
   },
   'expo-app': {
@@ -82,13 +80,13 @@ export const devDependencies = {
   },
   'next-app': {
     // nx/next 20 adds package that doesn't support eslint 9
-    'eslint-config-next': '^15.1.6',
+    'eslint-config-next': '^15.3.3',
     '@types/lodash-es': '^4.17.12',
   },
   'repo-config': {
-    syncpack: '^12.3.2',
+    syncpack: '^13.0.4',
   },
   auth: {
-    '@types/luxon': '3.4.2',
+    '@types/luxon': '3.6.2',
   },
 };
